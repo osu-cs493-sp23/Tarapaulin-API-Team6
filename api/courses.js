4283f856997df8aa6e17ce77b00e1afe83c3b474
methumk <methum.cg@gmail.com>
2023-06-10 14:28:07 -0700

    api endpoints

---
+++ api/courses.js
@@ -0,0 +1,36 @@
+const router = require('express').Router();
+
+exports.router = router
+
+/* 
+ * Route to get all courses (Paginated)
+ */
+router.get('/', async (req, res, next) => {
+
+})
+
+/* 
+ * Route to get course by Id
+ */
+router.get('/:id', async (req, res, next) => {
+
+})
+
+/* 
+ * Route to get students in a course
+ */
+router.get('/:id/students', async (req, res, next) => {
+
+})
+
+/* 
+ * Route to get assignments in a course
+ */
+router.get('/:id/assignments', async (req, res, next) => {
+
+})
+
+
+
+
+
--- api/index.js
+++ api/index.js
@@ -1,5 +1,9 @@
 const { Router } = require('express')
 const router = Router()
 
+router.use('/assignments', require('./assignments'))
+router.use('/courses', require('./courses'))
+router.use('/users', require('./users'))
+
 
 module.exports = router
