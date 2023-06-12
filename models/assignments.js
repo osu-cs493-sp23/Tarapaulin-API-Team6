e367948eea7a646696bc0b11e73d2c71888ca198
methumk <methum.cg@gmail.com>
2023-06-12 13:15:00 -0700

    most of assignment model set up

--- models/assignments.js
+++ models/assignments.js
@@ -1,10 +1,13 @@
-const db = require('../lib/sequelize')
-const { DataTypes } = require("sequelize")
+const { ObjectId } = require("mongodb")
+const { getDbReference } = require('../lib/mongo')
+const { extractValidFields } = require('../lib/validation')
 
-const assignmentSchemaSQL = {
-    title: { type: DataTypes.STRING, allowNull: false },
-    points: { type: DataTypes.INTEGER, allowNull: false },
-    due: { type: DataTypes.DATE, allowNull: false },
+const assignmentSchema = {
+    courseId: { required: true},
+    title: { required: true },
+    points: { required: true },
+    due: { required: true },
+    submissions: { required: true}
 }
 
 const assignmentFields = [
@@ -12,9 +15,103 @@ const assignmentFields = [
     'title',
     'points',
     'due',
+    'submissions'
 ]
 
-const Assignment = db.define('assignment', assignmentSchemaSQL)
+module.exports = { assignmentSchema, assignmentFields }
+
+/* 
+ * Returns null if id or assignment document is not found otherwise returns the assignment (without submission data)
+ */
+async function getAssignmentById(id){
+    const db = getDbReference()
+    const collection = db.collection('assignments')
+
+    let assignment = null
+    if (ObjectId.isValid(userId)){
+        // Projection to not include submissions
+        assignment = await collection.findOne({ _id: id}).project({submissions: 0})
+    }
+    return assignment
+}
+exports.getAssignmentById = getAssignmentById
+
+/* 
+ * Inserts a new assignment into the DB. Returns promise that resolves to the ID of the newly-created assignment
+ */
+async function insertNewAssignment(assignment){
+    user = extractValidFields(assignment, assignmentSchema)
+    const db = getDbReference()
+    const collection = db.collection('assignments')
+    const result = collection.insertOne(assignment)
+    return result.insertedId
+}
+exports.insertNewAssignment = insertNewAssignment
+
+/*
+ * Executes a DB query to bulk insert an array of new assignments into the database.
+ * Returns a Promise that resolves to a map of the IDs of the newly-created
+ * assignment entries.
+ */
+async function bulkInsertNewAssignment(assignments){
+    const assignmentsToInsert = assignments.map(function (assignment) {
+        return extractValidFields(assignment, assignmentSchema)
+    })
+
+    const db = getDbReference()
+    const collection = db.collection('assignments')
+    const result = await collection.insertMany(assignmentsToInsert)
+    return result.insertedIds
+}
+exports.bulkInsertNewAssignment = bulkInsertNewAssignment
+
+async function updateAssignmentById(id){
+    // TODO...
+}
+exports.updateAssignmentById = updateAssignmentById
+
+/* 
+ * Removes an assignment by id returns ta writeResult on successful delete
+ * if it failed to delete the writeResult should contain a 'writeConcernError" field
+ * If invalid objectId returns null
+ */
+async function removeAssignmentById(id){
+    const db = getDbReference()
+    const collection = db.collection('assignments')
+    
+    let result = null
+    if (ObjectId.isValid(id)){
+        result = collection.remove({_id: new ObjectId(id)})
+    }
+
+    return result
+}
+exports.removeAssignmentById = removeAssignmentById
+
+/* 
+ * Returns a list of submissions if an assignment Id is valid otherwise returns null
+ */
+async function getAssignmentSubmissionsById(id){
+    const db = getDbReference()
+    const collection = db.collection('assignments')
+
+    let subs = null
+    if (ObjectId.isValid(id)){
+        subs = await collection.aggregate([
+            { $match: { _id: new ObjectId(id) } },
+            { $project: { submissions: 1 } }
+        ]).toArray()
+        return subs[0]
+    }
+
+    return subs
+}
+exports.getAssignmentSubmissionsById = getAssignmentSubmissionsById
+
+async function insertSubmissionToAssignmentById(id){
+    // TODO: ... technically update
+}
+exports.insertSubmissionToAssignmentById = insertSubmissionToAssignmentById
 
 
-module.exports = { assignmentSchemaSQL, assignmentFields, Assignment }
+
