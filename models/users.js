f666c835408e46a05f2c069dae6c1fa2e99c38c7
methumk <methum.cg@gmail.com>
2023-06-12 12:14:12 -0700

    users converted to mongo

--- models/users.js
+++ models/users.js
@@ -1,21 +1,72 @@
-const db = require('../lib/sequelize')
-const { DataTypes } = require("sequelize")
+// CLEAN: const db = require('../lib/sequelize')
 
-const userSchemaSQL = {
-    name: { type: DataTypes.STRING, allowNull: false },
-    email: { type: DataTypes.STRING, allowNull: false },
-    password: { type: DataTypes.STRING, allowNull: false },
-    role: { type: DataTypes.ENUM(['admin', 'instructor', 'student']), allowNull: false }
+const { ObjectId } = require("mongodb")
+const { getDbReference } = require('../lib/mongo')
+const { extractValidFields } = require('../lib/validation')
+
+const userSchema = {
+    name: { required: true },
+    email: { required: true },
+    password: { required: true },
+    role: {  required: true }
 }
 
 const userFields = [
     'name',
     'email',
     'password',
-    'role'
+    'role',
+    'courses'
 ]
 
-const User = db.define('user', userSchemaSQL)
+module.exports = { userSchema, userFields }
 
+/* 
+ * Returns null if userId or user document is not found otherwise returns the user
+ */
+async function getUserById(userId){
+    const db = getDbReference()
+    const collection = db.collection('users')
 
-module.exports = { userSchemaSQL, userFields, User }
+    if (!ObjectId.isValid(userId)){
+        return null
+    }else{
+        const users = await collection.findOne({ "_id": userId})
+        if (users){
+            return users
+        }else{
+            return null
+        }
+    }
+}
+exports.getUserById = getUserById
+
+/* 
+ * Inserts a new uwsert into the DB. Returns promise that resolves to the ID of the newly-created user
+ */
+async function insertNewUser(user){
+    user = extractValidFields(user, userSchema)
+    const db = getDbReference()
+    const collection = db.collection('users')
+    const result = collection.insertOne(user)
+    return result.insertedId
+}
+exports.insertNewUser = insertNewUser
+
+/*
+ * Executes a DB query to bulk insert an array new users into the database.
+ * Returns a Promise that resolves to a map of the IDs of the newly-created
+ * user entries.
+ */
+async function bulkInsertNewUser(users){
+    const usersToInsert = users.map(function (user) {
+        return extractValidFields(user, userSchema)
+    })
+
+    const db = getDbReference()
+    const collection = db.collection('users')
+    const result = await collection.insertMany(usersToInsert)
+    return result.insertedIds
+}
+exports.bulkInsertNewUser = bulkInsertNewUser
+
