8389eb363b1e823e9478c70877cc81c178628bae
methumk <methum.cg@gmail.com>
2023-06-12 17:40:08 -0700

    made changes to mongo container

--- rmongo.bat
+++ rmongo.bat
@@ -1,3 +1,3 @@
 @echo off
+@REM Creates mongo image and runs its container
 docker container run -d --name mongo-server --network final-assignment -p 27017:27017 -e "MONGO_INITDB_ROOT_USERNAME=root" -e "MONGO_INITDB_ROOT_PASSWORD=password" mongo
-docker exec -it mongo-server /bin/bash
