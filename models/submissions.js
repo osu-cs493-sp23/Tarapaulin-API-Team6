ce0557fa5961df8e17662bc393276f90f10bb1fa
methumk <methum.cg@gmail.com>
2023-06-12 19:18:21 -0700

    updated schema for submissions

--- models/submissions.js
+++ models/submissions.js
@@ -1,32 +1,20 @@
-const db = require('../lib/sequelize')
-const { DataTypes } = require("sequelize")
 const { extractValidFields } = require('../lib/validation')
 const { ObjectId } = require("mongodb")
 const { getDbReference } = require('../lib/mongo')
 
-const submissionSchemaSQL = {
-    timestamp: { type: DataTypes.DATE, allowNull: false },
-    grade: { type: DataTypes.FLOAT, allowNull: true },
-    file: { type: DataTypes.STRING, allowNull: false },
+const submissionSchema = {
+    assignmentId: { required: true },
+    studentId: { required: true },
+    timestamp: { required: true },
+    grade: { required: false },
+    file: { required: true },
 }
 
-const submissionFields = [
-    'assignmentId',
-    'studentId',
-    'timestamp',
-    'grade',
-    'file',
-]
-
-const Submission = db.define('submission', submissionSchemaSQL)
- exports.submissionSchemaSQL = submissionSchemaSQL
- exports.submissionFields = submissionFields
- exports.Submission = Submission
-
+ exports.submissionSchema = submissionSchema
 
 
 /*
- * Executes a DB query to bulk insert an array of new assignments into the database.
+ * Executes a DB query to bulk insert an array of new submissions into the database.
  * Returns a Promise that resolves to a map of the IDs of the newly-created
  * assignment entries.
  */
