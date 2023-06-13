9df6ae1a519ca4e187d43510266b3d2fe2a1ee93
methumk <methum.cg@gmail.com>
2023-06-13 02:59:14 -0700

    mongo bat file will remove container before creating new one

--- rmongo.bat
+++ rmongo.bat
@@ -1,3 +1,4 @@
 @echo off
 @REM Creates mongo image and runs its container
+docker rm --force mongo-server
 docker container run -d --name mongo-server --network final-assignment -p 27017:27017 -e "MONGO_INITDB_ROOT_USERNAME=root" -e "MONGO_INITDB_ROOT_PASSWORD=password" mongo
